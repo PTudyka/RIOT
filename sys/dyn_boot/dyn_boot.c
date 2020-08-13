91f7130025d268bcb3a66a142f772cebaa18e7c6
ptudyka <p.tudyka@tu-bs.de>
2020-08-13 18:20:28 +0200

    add dyn_boot module

---
+++ sys/dyn_boot/dyn_boot.c
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2020 TU-BS IBR
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     module_dyn_boot
+ * @{
+ *
+ * @file
+ * @brief       Dynamic Boot Behaviour implementation
+ *
+ * @author      ptudyka <p.tudyka@tu-bs.de>
+ *
+ * @}
+ */
+
+#include "dyn_boot.h"
+
+/* Implementation of the module */
---
+++ sys/include/dyn_boot.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2020 TU-BS IBR
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @defgroup    sys_dyn_boot Dynamic Boot Behaviour
+ * @ingroup     sys
+ * @brief       Select and deselects module depending on current supply voltage
+ *
+ * @{
+ *
+ * @file
+ *
+ * @author      ptudyka <p.tudyka@tu-bs.de>
+ */
+
+#ifndef DYN_BOOT_H
+#define DYN_BOOT_H
+
+/* Add header includes here */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Declare the API of the module */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* DYN_BOOT_H */
+/** @} */
