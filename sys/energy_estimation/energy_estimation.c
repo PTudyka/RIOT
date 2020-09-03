a7d23dfa3b993700a5985e09f5734b498900dd0d
ptudyka <p.tudyka@tu-bs.de>
2020-09-03 19:36:10 +0200

    add (empty) module for energy estimation

---
+++ sys/energy_estimation/energy_estimation.c
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2020 TU BS IBR
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     module_energy_estimation
+ * @{
+ *
+ * @file
+ * @brief       On-line Energy Estimation implementation
+ *
+ * @author      ptudyka <p.tudyka@tu-bs.de>
+ *
+ * @}
+ */
+
+#include "energy_estimation.h"
+
+/* Implementation of the module */
---
+++ sys/include/energy_estimation.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2020 TU BS IBR
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @defgroup    sys_energy_estimation On-line Energy Estimation
+ * @ingroup     sys
+ * @brief       Estimates energy consumption of sensor node via estimation method by Dunkels et al.
+ *
+ * @{
+ *
+ * @file
+ *
+ * @author      ptudyka <p.tudyka@tu-bs.de>
+ */
+
+#ifndef ENERGY_ESTIMATION_H
+#define ENERGY_ESTIMATION_H
+
+/* Add header includes here */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Declare the API of the module */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ENERGY_ESTIMATION_H */
+/** @} */
