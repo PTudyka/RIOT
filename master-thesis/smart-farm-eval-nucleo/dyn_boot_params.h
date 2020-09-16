a57c59b365c375a52469733004d87db3328b49ff
ptudyka <p.tudyka@tu-bs.de>
2020-09-16 11:02:24 +0200

    copied params over

---
+++ master-thesis/smart-farm-eval-nucleo/dyn_boot_params.h
@@ -0,0 +1,132 @@
+/*
+ * Copyright (C) 2020 TU-BS IBR
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @defgroup    sys_dyn_boot Dynamic Boot Behaviour
+ * @ingroup     sys
+ * @brief       Select and deselects module depending on current supply voltage
+ * 
+ * Define which Modules to deactivate at specific run_level
+ * Sorted descending from highest run_level
+ * Example list:
+ * {
+ *  { RUN_LEVEL_7, DYN_BOOT_MODULE_PS },        \
+ *  { RUN_LEVEL_5, DYN_BOOT_MODULE_RANDOM },    \
+ *  { RUN_LEVEL_3, DYN_BOOT_MODULE_SAUL },      \
+ *  { RUN_LEVEL_3, DYN_BOOT_GNRC },             \
+ *  { RUN_LEVEL_0, MODULE_SHELL }               \
+ * }
+ * 
+ * Define Configuration to get V_Ref supply voltage via ADC
+ * struct: {
+ *  line;
+ *  res;
+ *  level0;
+ *  ...
+ *  level7;
+ * }
+ * 
+ * Define Configuration to get run_level via GPIO Pins
+ * struct: {
+ *  pin0;
+ *  pin2;
+ *  pin4;
+ * }
+ *
+ * @{
+ *
+ * @file
+ *
+ * @author      ptudyka <p.tudyka@tu-bs.de>
+ */
+
+#ifndef DYN_BOOT_PARAMS_H
+#define DYN_BOOT_PARAMS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef BOARD_INGA_RED
+/*
+#define RUN_LEVEL_MODULES {         \
+    { RUN_LEVEL_6, DYN_BOOT_MODULE_PRNG }, \
+    { RUN_LEVEL_3, DYN_BOOT_MODULE_ADXL345 }, \
+    { RUN_LEVEL_3, DYN_BOOT_MODULE_BMP180 }, \
+    { RUN_LEVEL_3, DYN_BOOT_MODULE_L3G4200D }, \
+    { RUN_LEVEL_2, DYN_BOOT_MODULE_GNRC }  \
+}
+*/
+// TODO: define array size as macro or will it be calculated at compile time?
+// #define RUN_LEVEL_MODULES_SIZE (1)
+
+// #define ADC_3_3_V   342
+// #define ADC_3_1_V   360
+// #define ADC_2_9_V   388
+// #define ADC_2_7_V   417
+// #define ADC_2_5_V   450
+// #define ADC_2_3_V   489
+// #define ADC_2_1_V   536
+// #define ADC_1_9_V   574
+// #define LEVEL_7_ADC 340 // 3,3V as Maximum
+#define LEVEL_0_ADC 549 // 1.9 V on HCS-3600
+#define LEVEL_1_ADC 522 // 2.0 V on HCS-3600
+#define LEVEL_2_ADC 478 // 2.2 V on HCS-3600
+#define LEVEL_3_ADC 439 // 2.4 V on HCS-3600
+#define LEVEL_4_ADC 406 // 2.6 V on HCS-3600
+#define LEVEL_5_ADC 377 // 2.8 V on HCS-3600
+#define LEVEL_6_ADC 359 // 3.0 V on HCS-3600
+#define LEVEL_7_ADC 345 // 3.2 V on HCS-3600
+// #define LEVEL_7_ADC 339 // 3.3 V on HCS-3600
+#define RES ADC_RES_10BIT
+#define LINE 0b11110
+
+/*
+#define DYN_BOOT_ADC_CONF {     \
+    LINE,       \
+    RES,        \
+    ADC_1_9_V,  \
+    ADC_2_1_V,  \
+    ADC_2_3_V,  \
+    ADC_2_5_V,  \
+    ADC_2_7_V,  \
+    ADC_2_9_V,  \
+    ADC_3_1_V,  \
+    ADC_3_3_V   \
+}
+*/
+#define DYN_BOOT_ADC_CONF {     \
+    LINE,       \
+    RES,        \
+    LEVEL_0_ADC,  \
+    LEVEL_1_ADC,  \
+    LEVEL_2_ADC,  \
+    LEVEL_3_ADC,  \
+    LEVEL_4_ADC,  \
+    LEVEL_5_ADC,  \
+    LEVEL_6_ADC,  \
+    LEVEL_7_ADC   \
+}
+
+/*
+#define DYN_BOOT_GPIO_CONF {    \
+    GPIO_PIN(PORT_C, 3),   \
+    GPIO_PIN(PORT_C, 4),    \
+    GPIO_PIN(PORT_C, 5)     \
+}
+*/
+
+#endif /* BOARD_INGA_RED */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* DYN_BOOT_PARAMS_H */
+/** @} */
--- master-thesis/smart-farm-eval-nucleo/main.c
+++ master-thesis/smart-farm-eval-nucleo/main.c
@@ -18,24 +18,38 @@
  * @}
  */
 
-// #include <stdio.h>
+#include <stdio.h>
 #include <stdbool.h>
 #include "dyn_boot.h"
 // #include "periph/pm.h"
 #include "ringbuffer.h"
 #include "net/gnrc.h"
 
+#include "board.h"
+// #include "gpio_measurement.h"
+
+/* Sensor specific includes */
+#include "bmp180.h"
+#include "bmp180_params.h"
+#include "phydat.h"
+
+#include "saul_reg.h"
+#include "periph/pm.h"
+
+/* Ringbuffer specific defines */
 #define RB_BUF_SIZE 16
-#define INT_SIZE (sizeof(int))
+#define DATA_SIZE (sizeof(sensor_data))
 
 /* States of smart farming application */
 typedef enum {
+    NODE_GET_RUN_LEVEL,
     NODE_MEASUREMENT,
     NODE_SEND_PACKET,
     NODE_SAVE_DATA,
-    NODE_SLEEP
+    NODE_SLEEP,
+    NODE_ERROR
 } node_status_t;
-static node_status_t next_status = NODE_MEASUREMENT;
+static node_status_t next_status = NODE_GET_RUN_LEVEL;
 
 /* Ringbuffer to save leftover packets */
 static char rb_buf[RB_BUF_SIZE];
@@ -48,53 +62,215 @@ static bool SEND_RINGBUFFER_PACKETS = false;
 static run_level_t current_run_level = RUN_LEVEL_7;
 
 /* Save read sensor data globally */
-static int sensor_data = 0;
-static char sensor_data_buf[INT_SIZE];
+static uint32_t sensor_data = 0;
+static phydat_t sensor_data_phy;
+static char sensor_data_buf[DATA_SIZE];
+
+static inline void pack_to_phydat(uint32_t pressure_val, phydat_t *res)
+{
+    res->val[0] = pressure_val / 100;
+    res->unit = UNIT_PA;
+    res->scale = 2;
+}
+
+void read_sensor_data(void)
+{
+    // Init sensor for reading
+    // bmp180_t dev;
+    // if(bmp180_init(&dev, &bmp180_params[0]))
+    // {
+    //     puts("Init bmp180 failed!");
+    //     next_status = NODE_ERROR;
+    //     return;
+    // }
+    // sensor_data = bmp180_read_pressure(&dev);
+
+    // /* Print as phy_dat_t for debug purpose */
+    // // phydat_t data;
+    // pack_to_phydat(sensor_data, &sensor_data_phy);
+    // phydat_dump(&sensor_data_phy, 1);
+
+    /* Read sensor data via SAUL */
+    puts("SAUL test application");
+    saul_reg_t *dev = saul_reg_find_type(SAUL_SENSE_TEMP);
+    if (dev == NULL) {
+        puts("No SAUL Pressure Device present");
+        return;
+    }
+    int dim = saul_reg_read(dev, &sensor_data_phy);
+    // printf("\nDev: %s\tType: %s\n", dev->name,
+    //         saul_class_to_str(dev->driver->type));
+    phydat_dump(&sensor_data_phy, dim);
+
+    // while (dev) {
+    //     dev = dev->next;
+    // }
+    // puts("\n##########################");
+}
+
+void send_packet(phydat_t *data)
+{
+    // Init send_thread for radio transmitting
+    gnrc_netif_t *netif = NULL;
+    if(!(netif = gnrc_netif_iter(netif))) {
+        puts("Unable to find netif");
+        next_status = NODE_ERROR;
+        return;
+    }
+
+    /// 0 Adress length means we want to use broadcast
+    size_t addr_len = 0;
+    uint8_t addr[GNRC_NETIF_L2ADDR_MAXLEN];
+    gnrc_pktsnip_t *pkt, *hdr;
+    gnrc_netif_hdr_t *nethdr;
+
+    /// Send packet as broadcast
+    uint8_t flags = 0 | GNRC_NETIF_HDR_FLAGS_BROADCAST;
+    
+    /// payload
+    char message[sizeof(phydat_t) +1]; // Sensor_data + \0 termination char
+    message[sizeof(phydat_t)] = '\0';
+
+    printf("Size of phydat_t: %d\n", sizeof(phydat_t));
+    memcpy(message, data, sizeof(phydat_t));
+    printf("Message to send: %s\n", message);
+
+    /// Build packet buffer
+    pkt = gnrc_pktbuf_add(NULL, message, sizeof(message), GNRC_NETTYPE_UNDEF);
+    if (pkt == NULL) {
+        puts("ERROR: packet buffer full");
+        return;
+    }
+
+    /// Build meassage header
+    hdr = gnrc_netif_hdr_build(NULL, 0, addr, addr_len);
+    if (hdr == NULL) {
+        puts("ERROR: packet buffer full");
+        gnrc_pktbuf_release(pkt);
+        return;
+    }
+    LL_PREPEND(pkt, hdr);
+    nethdr = (gnrc_netif_hdr_t *)hdr->data;
+    nethdr->flags = flags;
+    int ret = gnrc_netapi_send(netif->pid, pkt);
+    if (ret < 1) {
+        printf("[send_thread] unable to send: %d\n", ret);
+        gnrc_pktbuf_release(pkt);
+    } else {
+        puts("[send_thread] sent message");
+    }
+}
 
 int main(void)
 {
+    gpio_toggle(STARTUP_GPIO_PIN);
+    // toggle_start_timing();
+
     puts("Generated RIOT application: 'smart-farm-eval'");
+    printf("DATA_SIZE: %d\n", DATA_SIZE);
 
     while (1)
     {
-        // Get current run_level from startup
-        current_run_level = get_run_level();
+        // Delay for debugging via serial
+        xtimer_sleep(1);
 
-        // Check, if sensor node has enough energy to take a measurement
-        // Else go back to sleep
-        if (current_run_level < RUN_LEVEL_2)
-            next_status = NODE_SLEEP;
-        else
-            next_status = NODE_MEASUREMENT;
-        
         switch (next_status)
         {
+        case NODE_GET_RUN_LEVEL:
+            puts("Get_run_level reached");
+            // Get current run_level from startup
+            current_run_level = get_run_level();
+
+            printf("Current run_level: %d\n", current_run_level);
+
+            // Check, if sensor node has enough energy to take a measurement
+            // Else go back to sleep
+            if (current_run_level < RUN_LEVEL_2)
+                next_status = NODE_SLEEP;
+            else
+                next_status = NODE_MEASUREMENT;
+            break;
+
         case NODE_MEASUREMENT:
+            puts("NODE_MEASUREMENT reached");
             // TODO: Get measurement from Sensor (Temp, ...)
-            sensor_data = 0xFF;
+            // sensor_data = 0xFF;
+            read_sensor_data();
+
+            /* Set next status */
+            if (current_run_level > RUN_LEVEL_5)
+                next_status = NODE_SEND_PACKET;
+            else
+                next_status = NODE_SAVE_DATA;
             break;
 
         case NODE_SEND_PACKET:
-            // Send packet
+            puts("NODE_SEND_PACKET reached");
+            // Send only one packet and then move on
+            if (current_run_level == RUN_LEVEL_6)
+            {
+                send_packet(&sensor_data_phy);
+                break;
+            }
+            else
+            {
+                // At first send old packages
+                while (!ringbuffer_empty(&rb))
+                {
+                    // uint32_t data_buf = 0;
+                    // ringbuffer_get(&rb, (char*)&data_buf, 4);
+                    // printf("rb data: %u", data_buf);
+                    phydat_t data;
+                    ringbuffer_get(&rb, (char*)&data, sizeof(phydat_t));
+                    // pack_to_phydat(data_buf, &data);
+                    phydat_dump(&data, 1);
+                    send_packet(&data);
+                }
+            }
 
             // If current data != NULL, send this
             // Else check for SEND_MORE_DATA flag and check ringbuffer
             if (SEND_RINGBUFFER_PACKETS)
                 (void) SEND_RINGBUFFER_PACKETS;
+
+            next_status = NODE_SLEEP;
             break;
 
         case NODE_SAVE_DATA:
+            puts("NODE_SAVE_DATA reached");
             // Save Data to ringbuffer
-            for (unsigned int i=0; i < INT_SIZE; ++i)
-            {
-                sensor_data_buf[i] = (char)(((i * 8) >> sensor_data) & 0xFF);
-            }
-            ringbuffer_add(&rb, sensor_data_buf, INT_SIZE);
+            // Override oldest data, if bufer is full
+
+            /* Parse sensor data into byte array for ringbuffer packing */
+            // for (unsigned int i=0; i < DATA_SIZE; ++i)
+            // {
+            //     sensor_data_buf[i] = (char) ((sensor_data << (i)) & 0xFF);
+            //     // sensor_data_buf[i] = (char)(((i * 8) >> sensor_data) & 0xFF);
+            // }
+            // char buf[sizeof(phydat_t)];
+            (void) sensor_data_buf;
+            ringbuffer_add(&rb, (char*)&sensor_data_phy, sizeof(phydat_t));
+
+            printf("Ringbuffer free space: %d\n", ringbuffer_get_free(&rb));
+
+            // next_status = NODE_SLEEP;
+            next_status = NODE_SEND_PACKET;
             break;
 
         case NODE_SLEEP:
+            puts("NODE_SLEEP reached");
             // Set to sleep (lowest possibly power mode)
             // pm_set_lowest();
+            xtimer_sleep(5);
+
+            pm_reboot();
+
+            next_status = NODE_GET_RUN_LEVEL;
+            break;
+
+        case NODE_ERROR:
+            /* ERROR HANDLING */
+            puts("Error State reached!");
             break;
         } /* switch (next_status) */
     } /* while (1) */
